2a65df437add7c3bc82ef5c8ee1299ac98448840
이국진 <billlee297@gmail.com>
2025-04-25 16:38:50 +0900

    mod: 이미지 변경 및 애니메이션 수정

--- EnterTheTemple/main.js
+++ EnterTheTemple/main.js
@@ -5,7 +5,6 @@ let guardian4 = document.querySelector('.guardian.right.back');
 
 let temple = document.querySelector('.temple');
 let templeImg = document.querySelector('.temple img');
-let templeHill = document.querySelector('.temple-hill');
 
 window.addEventListener('scroll', () => {
   let scrollValue = window.scrollY;
@@ -13,29 +12,23 @@ window.addEventListener('scroll', () => {
 
   // 앞 줄 사대천왕
   guardian1.style.height = `${80 + scrollValue/80}vh`;
-  guardian1.style.left = `-${10 + scrollValue/10}%`;
-  guardian1.style.bottom = `-${scrollValue/15}%`;
+  guardian1.style.left = `-${scrollValue/10}%`;
+  guardian1.style.bottom = `-${scrollValue/10}%`;
 
   guardian3.style.height = `${80 + scrollValue/80}vh`;
-  guardian3.style.right = `-${10 + scrollValue/10}%`;
-  guardian3.style.bottom = `-${scrollValue/20}%`;
+  guardian3.style.right = `-${scrollValue/10}%`;
+  guardian3.style.bottom = `-${scrollValue/10}%`;
 
   // 뒷 줄 사대천왕
   guardian2.style.height = `${80 + scrollValue/80}vh`;
-  guardian2.style.left = `-${scrollValue/20}%`;
-  guardian2.style.bottom = `${20 - scrollValue/20}%`;
+  guardian2.style.left = `${10-scrollValue/15}%`;
+  guardian2.style.bottom = `${15 - scrollValue/15}%`;
 
   guardian4.style.height = `${80 + scrollValue/80}vh`;
-  guardian4.style.right = `-${scrollValue/20}%`;
-  guardian4.style.bottom = `${20 - scrollValue/20}%`;
+  guardian4.style.right = `${10-scrollValue/15}%`;
+  guardian4.style.bottom = `${15 - scrollValue/15}%`;
 
-  console.log("scrollValue :: ", scrollValue);
-  console.log("templeOffset :: ", templeOffset);
-  console.log("templeHill.clientHeight :: ", templeHill.clientHeight);
-
-  if(scrollValue * 2 >= templeOffset) {
-    temple.classList.add('active');
-  } else {
-    temple.classList.remove('active');
-  }
+  let templeOpacity = Math.min(1, (1 - ((window.innerHeight - scrollValue) / 1000)));
+  templeImg.style.opacity = `${templeOpacity.toFixed(2)}`;
+  templeImg.style.scale = `${templeOpacity}`;
 })
